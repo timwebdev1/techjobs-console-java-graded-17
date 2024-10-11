4627d28cc26606bec2a00b9a37efe8f3b5ed3c9b
timwebdev1 <tlmcdermott79@gmail.com>
2024-10-10 20:01:00 -0500

    passes 2 tests

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,12 +120,16 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
+        if (someJobs.size() == 0) {
+            System.out.print("No Results");
+        } else {
             for (HashMap<String, String> someJob : someJobs) {
-                    System.out.println("\n"+"*****");
-                    for (Map.Entry<String, String> jobSelection : someJob.entrySet()) {
-                        System.out.println(jobSelection.getKey() + ": " + jobSelection.getValue());
-                    }
-                    System.out.println("*****");
+                System.out.println("\n" + "*****");
+                for (Map.Entry<String, String> jobSelection : someJob.entrySet()) {
+                    System.out.println(jobSelection.getKey() + ": " + jobSelection.getValue());
+                }
+                System.out.println("*****");
             }
+        }
     }
 }
